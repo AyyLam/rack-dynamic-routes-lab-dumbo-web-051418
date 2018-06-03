abb9070695196274725170faec5651f687139b11
Anthony Lam <al3101@nyu.edu>
2018-06-03 01:54:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/) 
       item_req = req.path.split("/items/").last 
       if @@items.find{|i| i.name == item_req} 
-        resp.write "#{i.price}" 
+        resp.write "#{i}" 
       else 
         resp.status = 400
         resp.write "Item not found"
